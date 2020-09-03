633aeb6d41225a31c52398d66dbc209899f9c8eb
sid5233 <sudarshan.bhamare@gmail.com>
2020-09-03 19:30:28 +0530

    register update

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HttpClient  } from '@angular/common/http';
 
+
+
+import { from } from 'rxjs';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  public fbFormGroup = this.fb.group({
+    username : ['', Validators.required],
+    password : ['', Validators.required],
+    email : ['', Validators.required],
+    mobile : ['', Validators.required]
+  });
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
 
+  constructor ( 
+    private fb : FormBuilder, 
+    private router : Router , 
+    private http : HttpClient) {}
+
+  ngOnInit(): void {}
+   async register(){
+     const data = this.fbFormGroup.value;
+     const url = "http://localhost:3000/adduser";
+
+     await this.http.post(url,data).toPromise();
+     this.router.navigate(['login']);
+   }
 }
