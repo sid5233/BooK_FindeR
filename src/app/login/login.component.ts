d79f409fb751013193e3df3a9df14825618e855e
sid5233 <sudarshan.bhamare@gmail.com>
2020-09-04 15:21:57 +0530

    login sign up success

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,6 +3,8 @@ import { faBookReader} from '@fortawesome/free-solid-svg-icons';
 import { } from '@fortawesome/free-regular-svg-icons';
 import {FormBuilder, Validators} from '@angular/forms'
 import { Router } from '@angular/router';
+import { HttpClient  } from '@angular/common/http';
+
  
 @Component({
   selector: 'app-login',
@@ -16,21 +18,38 @@ export class LoginComponent implements OnInit {
   public fbFormGroup = this.fb.group({
       username: ['', Validators.required],
       password: ['',Validators.required],
+      email : ['', Validators.required],
+      mobile : ['', Validators.required],
   });
-  constructor( private fb : FormBuilder,private router : Router) { }
+
+  public fbFormGroup1 = this.fb.group({
+    username: ['', Validators.required],
+    password: ['',Validators.required],
+});
+
+  constructor( private fb : FormBuilder,private router : Router ,
+    private http : HttpClient ) { }
 
   ngOnInit(): void {
   }
 
   login() {
-    const data = this.fbFormGroup.value;
+    const data = this.fbFormGroup1.value;
     if (data.username === 'angular' && data.password === 'admin'){
       sessionStorage.setItem('sid','true');
       this.router.navigate(['homepage']);
     }
     else { 
-      this.uiInvalidCredintial = true; 
+   
     }
+  }
+
+  async register(){
+    const data = this.fbFormGroup.value;
+    const url = "http://localhost:3000/adduser";
+
+    await this.http.post(url,data).toPromise();
+    this.router.navigate(['homepage']);
   }
 
 }
